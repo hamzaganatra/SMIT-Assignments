a889da8b2ad23748c91f32308354850cb75becba
hamzaganatra <hamzaganatra@gmail.com>
2025-11-26 23:56:48 +0500

    The start of Java script

---
+++ sir/jv_start/script.js
@@ -0,0 +1,10 @@
+//var A = 18 / (3+3) * 4 - {12 - [ 5 * (2 + 1) ]} + 7^2;
+// Bracket Open, Divide, Multiply, Addition, Substraction
+var B1 = 6;
+var B2 = 3;
+var division = 18 / B1;
+var lhs = division * 4;
+var square = 49;
+var nlhs = B2 + lhs;
+var result = square + nlhs;
+console.log(result);
