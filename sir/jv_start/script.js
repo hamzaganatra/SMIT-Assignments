8fb7f722d917efcb198408dbe8871fa2571f36bb
hamzaganatra <hamzaganatra@gmail.com>
2025-12-05 23:49:20 +0500

    alert and prompt assignment

--- sir/jv_start/script.js
+++ sir/jv_start/script.js
@@ -1,10 +1,3 @@
-//var A = 18 / (3+3) * 4 - {12 - [ 5 * (2 + 1) ]} + 7^2;
-// Bracket Open, Divide, Multiply, Addition, Substraction
-var B1 = 6;
-var B2 = 3;
-var division = 18 / B1;
-var lhs = division * 4;
-var square = 49;
-var nlhs = B2 + lhs;
-var result = square + nlhs;
-console.log(result);
+const age = prompt("what is your age");
+const Name = prompt("what is your name");
+alert("congratulations " + Name + "you are " + age + " years old" );
